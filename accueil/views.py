77cd24dafed568ce78ef4c225da2432ed37f9e9b
Jireh <jireh.t@h2altitude.com>
2025-12-22 13:06:07 +0100

    Modif: Mise à jour du texte sur la page d'accueil

--- accueil/views.py
+++ accueil/views.py
@@ -1,10 +1,10 @@
 
 from django.shortcuts import render
-from datetime import date
+from datetime import date, datetime
 from .forms import NomForm
 
 def index(request):
-    message = "Bienvenue sur mon site Django !"
+    message = "Salut et Bienvenue à tous sur mon super site Django !"
     nom = None
 
     if request.method == "POST":
@@ -15,9 +15,25 @@ def index(request):
     else:
         form = NomForm()
 
+    # Détermination du message par défaut selon l'heure (si aucun nom soumis)
+    if not nom:
+        current_hour = datetime.now().hour
+        if current_hour < 12:
+            message = "Bon matin !"
+        else:
+            message = "Bon après-midi !"
+
+    # Exemple simple de liste de tâches à passer au template
+    tasks = [
+        {'title': 'Apprendre Django', 'done': False},
+        {'title': 'Écrire des tests', 'done': True},
+        {'title': "Déployer l'application", 'done': False},
+    ]
+
     contexte = {
         'message': message,
         'date_du_jour': date.today(),
-        'form': form
+        'form': form,
+        'tasks': tasks,
     }
     return render(request, 'accueil/index.html', contexte)
