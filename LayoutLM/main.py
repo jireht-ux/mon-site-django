82b08b3426422f3a9b54383b62b653deecd8a660
Jireh <jireh.t@h2altitude.com>
2026-01-23 15:31:36 +0100

    test facture facture_24

--- LayoutLM/main.py
+++ LayoutLM/main.py
@@ -112,4 +112,4 @@ def process_hybrid_invoice_v4(pdf_path):
     return final_report
 
 # Exécution
-print(json.dumps(process_hybrid_invoice_v4("Modele de facture.pdf"), indent=4, ensure_ascii=False))
+print(json.dumps(process_hybrid_invoice_v4("facture_24.pdf"), indent=4, ensure_ascii=False))
