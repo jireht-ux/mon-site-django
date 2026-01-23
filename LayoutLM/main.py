ccd3faa1254b8ec4906dac913698a97bfa9b4748
Jireh <jireh.t@h2altitude.com>
2026-01-23 17:01:56 +0100

    test Copy of Modele-facture-Kafeo

--- LayoutLM/main.py
+++ LayoutLM/main.py
@@ -112,4 +112,4 @@ def process_hybrid_invoice_v6(pdf_path):
         }
     return final_report
 
-print(json.dumps(process_hybrid_invoice_v6("facture_8_2.pdf"), indent=4, ensure_ascii=False))
+print(json.dumps(process_hybrid_invoice_v6("Copy of Modele-facture-Kafeo.pdf"), indent=4, ensure_ascii=False))
