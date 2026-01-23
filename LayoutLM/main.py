a17ae4d5199756fdf0072f73c42e9a25084a6586
Jireh <jireh.t@h2altitude.com>
2026-01-23 13:15:16 +0100

    amelioration aggregation de micro-macro-zone2

--- LayoutLM/main.py
+++ LayoutLM/main.py
@@ -112,4 +112,4 @@ def process_hybrid_invoice_v4(pdf_path):
     return final_report
 
 # Exécution
-print(json.dumps(process_hybrid_invoice_v4("facture_2.pdf"), indent=4, ensure_ascii=False))
+print(json.dumps(process_hybrid_invoice_v4("facture_FAC-2025-00001_2.pdf"), indent=4, ensure_ascii=False))
